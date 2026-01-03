b994fd7dcd612ce3dc17f6f534f84579d6cb6ab3
espmancer <aidenpike113@gmail.com>
2026-01-03 15:18:23 -0500

    Move code from main to EventHandler

--- sample/EventHandler.py
+++ sample/EventHandler.py
@@ -8,5 +8,41 @@ from Player import Player
 
 class EventHandler():
     def __init__(self):
-        self.obstacleObject = Obstacle()
-        self.playerObject = Player()
+        pygame.init()
+        
+        self.obstacleObject = Obstacle(1, 1)
+        self.playerObject = Player(1,1,1,1)
+
+        screen = pygame.display.set_mode([300,800])
+        running = True
+
+        while running:
+            # Poll through all events.
+            for event in pygame.event.get():
+                # Did the player close the window?
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Define a light blue background.
+            backgroundColor = (144,213,255)
+
+            # Color in the background. 
+            screen.fill(backgroundColor)
+
+            # Define a black circle to represent the player.
+            playerColor = (0,0,0)
+            playerRadius = 25
+            playerX = 250
+            playerY = 250
+            playerCenter = (playerX, playerY)
+
+            # Draw the player.
+            pygame.draw.circle(screen, playerColor, playerCenter, playerRadius)
+
+            # Flip the display.
+            pygame.display.flip()
+
+        pygame.quit()
+      
+if __name__ == "__main__":
+    pass
--- sample/main.py
+++ sample/main.py
@@ -1,34 +1,6 @@
-import pygame
+from EventHandler import EventHandler
 
-pygame.init()
+def main():
+    eventHandler = EventHandler()
 
-screen = pygame.display.set_mode([300,800])
-running = True
-
-while running:
-    # Poll through all events.
-    for event in pygame.event.get():
-        # Did the player close the window?
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Define a light blue background.
-    backgroundColor = (144,213,255)
-
-    # Color in the background. 
-    screen.fill(backgroundColor)
-
-    # Define a black circle to represent the player.
-    playerColor = (0,0,0)
-    playerRadius = 25
-    playerX = 250
-    playerY = 250
-    playerCenter = (playerX, playerY)
-
-    # Draw the player.
-    pygame.draw.circle(screen, playerColor, playerCenter, playerRadius)
-
-    # Flip the display.
-    pygame.display.flip()
-
-pygame.quit()
+main()
