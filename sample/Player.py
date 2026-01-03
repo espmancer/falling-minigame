f4a3994b357d4cdd818194cbe8f568fc21be0dc0
espmancer <aidenpike113@gmail.com>
2026-01-02 19:12:11 -0500

    Create getter and setters

--- sample/Player.py
+++ sample/Player.py
@@ -8,8 +8,28 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, health, fallSpeed, strafeSpeed, rotateSpeed):
         super().__init__
 
-        this.health = health
-        this.fallSpeed = fallSpeed
-        this.strafeSpeed = strafeSpeed
-        this.rotateSpeed = rotateSpeed
+        self.health = health
+        self.fallSpeed = fallSpeed
+        self.strafeSpeed = strafeSpeed
+        self.rotateSpeed = rotateSpeed
+
+    # Get the health of the player.
+    def getHealth(self) -> int:
+        return self.health
+
+    # Set the health of the player.
+    def setHealth(self, health):
+        self.health = health
+
+    # Set the fall speed of the player.
+    def setFallSpeed(self, fallSpeed):
+        self.fallSpeed = fallSpeed
+    
+    # Set the strafe speed of the player.
+    def setStrafeSpeed(self, strafeSpeed):
+        self.strafeSpeed = strafeSpeed
+    
+    # Set the rotate speed of the player.
+    def setRotateSpeed(self, rotateSpeed):
+        self.rotateSpeed = rotateSpeed
     
