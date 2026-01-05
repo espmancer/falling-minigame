9c5eaf5c1fd8d6bbe3ad1fa51b6be843396e426e
espmancer <aidenpike113@gmail.com>
2026-01-04 23:01:17 -0500

    Move Background logic to Background class

--- sample/EventHandler.py
+++ sample/EventHandler.py
@@ -5,6 +5,7 @@ This class will handle all game events by calling each appropriate class method.
 import pygame
 from Obstacle import Obstacle
 from Player import Player
+from Background import Background
 
 class EventHandler():
     def __init__(self):
@@ -12,33 +13,30 @@ class EventHandler():
         
         self.obstacleObject = Obstacle(1, 1)
         self.playerObject = Player(1,1,1,1)
+        # Define a light blue background.
+        backgroundColor = (144,213,255)
+        self.backgroundObject = Background(backgroundColor)
 
         screen = pygame.display.set_mode([300,800])
+        self.playerObject.setPosition((150,0))
         running = True
 
         while running:
+            # Make the player fall.
+            newX = self.playerObject.getPosition()[0]
+            newY = self.playerObject.getPosition()[1] + 0.1
+            newPosition = (newX, newY)
+
+            self.playerObject.setPosition(newPosition)
             # Poll through all events.
             for event in pygame.event.get():
                 # Did the player close the window?
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Define a light blue background.
-            backgroundColor = (144,213,255)
-
-            # Color in the background. 
-            screen.fill(backgroundColor)
-
-            # Define a black circle to represent the player.
-            playerColor = (0,0,0)
-            playerRadius = 25
-            playerX = 250
-            playerY = 250
-            playerCenter = (playerX, playerY)
-
-            # Draw the player.
-            pygame.draw.circle(screen, playerColor, playerCenter, playerRadius)
-
+            # Draw all objects.
+            screen.fill(self.backgroundObject.getBackground())
+            screen.blit(self.playerObject.image, self.playerObject.rect)
             # Flip the display.
             pygame.display.flip()
 
