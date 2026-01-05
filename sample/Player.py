704b333960607f5cc66abd43e942995f5a6a0986
espmancer <aidenpike113@gmail.com>
2026-01-04 23:00:43 -0500

    Add sprite and position

--- sample/Player.py
+++ sample/Player.py
@@ -5,17 +5,27 @@ This class represents the health and fall, strafe, and rotate speed of the playe
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, health, fallSpeed, strafeSpeed, rotateSpeed):
-        super().__init__
+    def __init__(self, health, fallSpeed, strafeSpeed, rotateSpeed, position=(0.0,0.0)):
+        super().__init__()
 
         self.health = health
         self.fallSpeed = fallSpeed
         self.strafeSpeed = strafeSpeed
         self.rotateSpeed = rotateSpeed
+        self.image = pygame.Surface([10,15])
+
+        self.image.fill((0,0,0))
+
+        self.rect = self.image.get_rect()
+        (self.rect.x, self.rect.y) = position
 
     # Get the health of the player.
     def getHealth(self) -> int:
         return self.health
+    
+    # Get the position of the player.
+    def getPosition(self) -> tuple:
+        return (self.rect.x, self.rect.y)
 
     # Set the health of the player.
     def setHealth(self, health):
@@ -32,4 +42,8 @@ class Player(pygame.sprite.Sprite):
     # Set the rotate speed of the player.
     def setRotateSpeed(self, rotateSpeed):
         self.rotateSpeed = rotateSpeed
+
+    # Set the position of the player.
+    def setPosition(self, position):
+        (self.rect.x, self.rect.y) = position
     
