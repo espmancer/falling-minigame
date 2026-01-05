a37210f97d425c38ed300c59efa33e4d3b36804b
espmancer <aidenpike113@gmail.com>
2026-01-04 23:01:24 -0500

    Fix super() call

--- sample/Obstacle.py
+++ sample/Obstacle.py
@@ -6,7 +6,7 @@ import pygame
 
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, damage, riseSpeed):
-        super().__init__
+        super().__init__()
         
         self.damage = damage
         self.riseSpeed = riseSpeed
